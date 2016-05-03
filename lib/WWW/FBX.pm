e227e552aedf644356c83bc417377caae5e8ed43
Laurent Kislaire <teebeenator@gmail.com>
2016-05-04 00:21:38 +0200

    That'd be 0.05

--- lib/WWW/FBX.pm
+++ lib/WWW/FBX.pm
@@ -15,7 +15,7 @@ with 'WWW::FBX::Role::Auth';
  
 use namespace::autoclean;
 
-our $VERSION="0.04";
+our $VERSION="0.05";
 
 has lwp_args    => ( isa => 'HashRef', is => 'ro', default => sub { {} } );
 has ua          => ( isa => 'LWP::UserAgent', is => 'rw', lazy => 1, builder => '_build_ua' );
