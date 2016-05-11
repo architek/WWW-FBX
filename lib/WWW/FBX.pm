5729a5e32b16cc67ce35b811bb40eb995e9c8a66
Laurent Kislaire <teebeenator@gmail.com>
2016-05-12 01:12:23 +0200

    v0.13

--- lib/WWW/FBX.pm
+++ lib/WWW/FBX.pm
@@ -15,7 +15,7 @@ with 'WWW::FBX::Role::Auth';
  
 use namespace::autoclean;
 
-our $VERSION = "0.12";
+our $VERSION = "0.13";
 
 has base_url    => ( isa => 'Str', is => 'ro', default => 'http://mafreebox.free.fr' );
 has lwp_args    => ( isa => 'HashRef', is => 'ro', default => sub { {} } );
